1f331d1cd5ca683c40b66c2d94928ca33d520796
Nuno <nm.nogueira@campus.fct.unl.pt>
2025-05-15 23:46:05 +0000

    Updated compose and input prep scripts

--- mp-spdz/scripts/iprep.sh
+++ mp-spdz/scripts/iprep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# input preparation script
 set -e
 
 generate=false
@@ -8,10 +8,15 @@ destination_dir="MP-SPDZ/Player-Data"
 generated_data_dir="data/"
 
 usage() {
-    echo "Usage: $0 [-g] [-c] <program_name>"
+    echo "Usage: $0 [-g] [-c] <program_name> [<geninput_args>]"
     echo "  -g                Generate data"
     echo "  -c                Copy generated data"
     echo "  <program_name>    Name of the program to run"
+    echo "  [<geninput_args>] Additional arguments for geninput.py"
+    echo ""
+    echo "  Generator script arguments:"
+    echo "    -l <num_rows>   Specify the number of rows for the generated data (default: 10)"
+    echo "    -n <num_bits>   Specify the number of bits for each number (default: 32)"
     exit 1
 }
 
@@ -28,7 +33,18 @@ shift $((OPTIND - 1))
 
 program_name=$1
 
+if [ -z "$program_name" ]; then
+    usage
+fi
+if [ "$generate" = false ] && [ "$copy" = false ]; then
+    echo "At least one of -g or -c must be specified."
+    usage
+fi
+
+shift
+
 if [ "$generate" = true ]; then
+    python scripts/geninput.py -e $program_name $@
     echo "Generated data for <$program_name>"
 fi
 
